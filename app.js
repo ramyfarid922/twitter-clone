ed75363c195d489a814df549b071ba382040fc67
Ramy <ramyfarid922@gmail.com>
2021-02-18 19:32:25 +0200

    Connecting to postgres on docker instance

---
+++ app.js
@@ -0,0 +1,27 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  user: "postgres",
+  password: "postgres",
+  port: 5432,
+  host: "172.17.0.2",
+  database: "twitter",
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected successfully!");
+  })
+  .then(() => {
+    return client.query("SELECT * FROM public.dummy_table");
+  })
+  .then((content) => {
+    console.table(content.rows);
+  })
+  .catch((e) => {
+    console.log(e);
+  })
+  .finally(() => {
+    client.end();
+  });
